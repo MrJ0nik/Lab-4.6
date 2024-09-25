4e4bfc589dca9c4ecc3dc7a098e3855c036e7fd9
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-09-25 20:25:01 +0300

    first commit

---
+++ lab 4.6/lab 4.6.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double P, S = 0;
+
+    P = 1;
+    int i = 1, k = 1;
+    while (i <= 10) {
+        P = 1;
+        k = 1;
+        while (k <= i) {
+            P *= k / i;
+            k++;
+        }
+        S += (1 + P) / (i * i);
+        i++;
+    }
+    cout << S << endl;
+
+    S = 0;
+
+    i = 1;
+    do {
+        P = 1;
+        k = 1;
+        do {
+            P *= k / i;
+            k++;
+        } while (k <= i);
+        S += (1 + P) / (i * i);
+        i++;
+    } while (i <= 10);
+    cout << S << endl;
+
+    S = 0;
+
+    for (i = 1; i <= 10; i++) {
+        P = 1;
+        for (k = 1; k <= i; k++) {
+            P *= k / i;
+        }
+        S += (1 + P) / (i * i);
+    }
+    cout << S << endl;
+
+    S = 0;
+
+    for (i = 10; i >= 1; i--) {
+        P = 1;
+        for (k = 1; k <= i; k++) {
+            P *= k / i;
+        }
+        S += (1 + P) / (i * i);
+    }
+    cout << S << endl;
+
+    return 0;
+}
